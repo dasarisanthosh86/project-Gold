a47a70a4b036e3069ba18fefd1f18e01d01bb1ee
Santhosh <dasarisanthosh8688@gmail.com>
2025-06-27 13:57:31 +0530

    index.html

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ function sanThosh(event) {
   
   if (username === "admin" && password === "1234") {
     
-    window.location.href = "earring.html";
+    window.location.href = "calculation.html";
   }
 }
 
+function calculateLoan(){
+        let weight = parseFloat(document.getElementById("weight").value);
+        
+        let purity = parseFloat(document.getElementById("purity").value);
+       
+          purityfraction = (purity / 24);
+
+        totalValue = weight * 10000 * purityfraction;
+        loanAmount = (totalValue * 50 )/100; 
+        intrest = ( loanAmount *2)/100;
+        
+           document.getElementById('result').innerText =
+             "Eligible Amount " + loanAmount ;
+             document.getElementById('intrest').innerText =
+             " Intrest"  + intrest;
+             document.getElementById('result').style.color = "green"
+             
+         }
